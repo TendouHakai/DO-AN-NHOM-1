bd24d3a485bc543a6400e35a568d111f85ff3e43
Triệu Tuấn Tú <90883273+Vippro67@users.noreply.github.com>
2021-10-30 21:04:12 +0700

    Hàng Hoá
    Chức năng thêm xoá sửa, xem chi tiết

---
+++ DO-AN-NHOM-1/App_sale_manager/App_sale_manager/Form_ChiTietHH.Designer.cs
@@ -0,0 +1,291 @@
+﻿
+namespace App_sale_manager
+{
+    partial class Form_ChiTietHH
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblLoaiID = new System.Windows.Forms.Label();
+            this.lblSLToiThieu = new System.Windows.Forms.Label();
+            this.lblGiaBan = new System.Windows.Forms.Label();
+            this.lblDVT = new System.Windows.Forms.Label();
+            this.lblGiaNhap = new System.Windows.Forms.Label();
+            this.lblSoLuong = new System.Windows.Forms.Label();
+            this.lblNuocSX = new System.Windows.Forms.Label();
+            this.lblTenSP = new System.Windows.Forms.Label();
+            this.lblMaSP = new System.Windows.Forms.Label();
+            this.lblMoTa = new System.Windows.Forms.Label();
+            this.lblNDMaSP = new System.Windows.Forms.Label();
+            this.lblNDTenSP = new System.Windows.Forms.Label();
+            this.lblNDDVT = new System.Windows.Forms.Label();
+            this.lblNDNuocSX = new System.Windows.Forms.Label();
+            this.lblNDLoaiID = new System.Windows.Forms.Label();
+            this.lblNDSoLuong = new System.Windows.Forms.Label();
+            this.lblNDSLToiThieu = new System.Windows.Forms.Label();
+            this.lblNDGiaBan = new System.Windows.Forms.Label();
+            this.lblNDGiaNhap = new System.Windows.Forms.Label();
+            this.lblNDMoTa = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblLoaiID
+            // 
+            this.lblLoaiID.AutoSize = true;
+            this.lblLoaiID.Location = new System.Drawing.Point(30, 248);
+            this.lblLoaiID.Name = "lblLoaiID";
+            this.lblLoaiID.Size = new System.Drawing.Size(35, 17);
+            this.lblLoaiID.TabIndex = 48;
+            this.lblLoaiID.Text = "Loại";
+            // 
+            // lblSLToiThieu
+            // 
+            this.lblSLToiThieu.AutoSize = true;
+            this.lblSLToiThieu.Location = new System.Drawing.Point(30, 360);
+            this.lblSLToiThieu.Name = "lblSLToiThieu";
+            this.lblSLToiThieu.Size = new System.Drawing.Size(118, 17);
+            this.lblSLToiThieu.TabIndex = 47;
+            this.lblSLToiThieu.Text = "Số lượng tối thiểu";
+            // 
+            // lblGiaBan
+            // 
+            this.lblGiaBan.AutoSize = true;
+            this.lblGiaBan.Location = new System.Drawing.Point(30, 416);
+            this.lblGiaBan.Name = "lblGiaBan";
+            this.lblGiaBan.Size = new System.Drawing.Size(58, 17);
+            this.lblGiaBan.TabIndex = 46;
+            this.lblGiaBan.Text = "Giá bán";
+            // 
+            // lblDVT
+            // 
+            this.lblDVT.AutoSize = true;
+            this.lblDVT.Location = new System.Drawing.Point(30, 136);
+            this.lblDVT.Name = "lblDVT";
+            this.lblDVT.Size = new System.Drawing.Size(75, 17);
+            this.lblDVT.TabIndex = 45;
+            this.lblDVT.Text = "Đơn vị tính";
+            // 
+            // lblGiaNhap
+            // 
+            this.lblGiaNhap.AutoSize = true;
+            this.lblGiaNhap.Location = new System.Drawing.Point(30, 472);
+            this.lblGiaNhap.Name = "lblGiaNhap";
+            this.lblGiaNhap.Size = new System.Drawing.Size(66, 17);
+            this.lblGiaNhap.TabIndex = 44;
+            this.lblGiaNhap.Text = "Giá nhập";
+            // 
+            // lblSoLuong
+            // 
+            this.lblSoLuong.AutoSize = true;
+            this.lblSoLuong.Location = new System.Drawing.Point(30, 304);
+            this.lblSoLuong.Name = "lblSoLuong";
+            this.lblSoLuong.Size = new System.Drawing.Size(64, 17);
+            this.lblSoLuong.TabIndex = 43;
+            this.lblSoLuong.Text = "Số lượng";
+            // 
+            // lblNuocSX
+            // 
+            this.lblNuocSX.AutoSize = true;
+            this.lblNuocSX.Location = new System.Drawing.Point(30, 192);
+            this.lblNuocSX.Name = "lblNuocSX";
+            this.lblNuocSX.Size = new System.Drawing.Size(98, 17);
+            this.lblNuocSX.TabIndex = 42;
+            this.lblNuocSX.Text = "Nước sản xuất";
+            // 
+            // lblTenSP
+            // 
+            this.lblTenSP.AutoSize = true;
+            this.lblTenSP.Location = new System.Drawing.Point(30, 80);
+            this.lblTenSP.Name = "lblTenSP";
+            this.lblTenSP.Size = new System.Drawing.Size(99, 17);
+            this.lblTenSP.TabIndex = 41;
+            this.lblTenSP.Text = "Tên sản phẩm";
+            // 
+            // lblMaSP
+            // 
+            this.lblMaSP.AutoSize = true;
+            this.lblMaSP.Location = new System.Drawing.Point(30, 24);
+            this.lblMaSP.Name = "lblMaSP";
+            this.lblMaSP.Size = new System.Drawing.Size(93, 17);
+            this.lblMaSP.TabIndex = 40;
+            this.lblMaSP.Text = "Mã sản phẩm";
+            // 
+            // lblMoTa
+            // 
+            this.lblMoTa.AutoSize = true;
+            this.lblMoTa.Location = new System.Drawing.Point(378, 24);
+            this.lblMoTa.Name = "lblMoTa";
+            this.lblMoTa.Size = new System.Drawing.Size(43, 17);
+            this.lblMoTa.TabIndex = 49;
+            this.lblMoTa.Text = "Mô tả";
+            // 
+            // lblNDMaSP
+            // 
+            this.lblNDMaSP.AutoSize = true;
+            this.lblNDMaSP.Location = new System.Drawing.Point(172, 24);
+            this.lblNDMaSP.Name = "lblNDMaSP";
+            this.lblNDMaSP.Size = new System.Drawing.Size(46, 17);
+            this.lblNDMaSP.TabIndex = 50;
+            this.lblNDMaSP.Text = "label1";
+            // 
+            // lblNDTenSP
+            // 
+            this.lblNDTenSP.AutoSize = true;
+            this.lblNDTenSP.Location = new System.Drawing.Point(172, 80);
+            this.lblNDTenSP.Name = "lblNDTenSP";
+            this.lblNDTenSP.Size = new System.Drawing.Size(46, 17);
+            this.lblNDTenSP.TabIndex = 51;
+            this.lblNDTenSP.Text = "label2";
+            // 
+            // lblNDDVT
+            // 
+            this.lblNDDVT.AutoSize = true;
+            this.lblNDDVT.Location = new System.Drawing.Point(172, 136);
+            this.lblNDDVT.Name = "lblNDDVT";
+            this.lblNDDVT.Size = new System.Drawing.Size(46, 17);
+            this.lblNDDVT.TabIndex = 52;
+            this.lblNDDVT.Text = "label3";
+            // 
+            // lblNDNuocSX
+            // 
+            this.lblNDNuocSX.AutoSize = true;
+            this.lblNDNuocSX.Location = new System.Drawing.Point(172, 192);
+            this.lblNDNuocSX.Name = "lblNDNuocSX";
+            this.lblNDNuocSX.Size = new System.Drawing.Size(46, 17);
+            this.lblNDNuocSX.TabIndex = 53;
+            this.lblNDNuocSX.Text = "label4";
+            // 
+            // lblNDLoaiID
+            // 
+            this.lblNDLoaiID.AutoSize = true;
+            this.lblNDLoaiID.Location = new System.Drawing.Point(172, 248);
+            this.lblNDLoaiID.Name = "lblNDLoaiID";
+            this.lblNDLoaiID.Size = new System.Drawing.Size(46, 17);
+            this.lblNDLoaiID.TabIndex = 54;
+            this.lblNDLoaiID.Text = "label5";
+            // 
+            // lblNDSoLuong
+            // 
+            this.lblNDSoLuong.AutoSize = true;
+            this.lblNDSoLuong.Location = new System.Drawing.Point(172, 304);
+            this.lblNDSoLuong.Name = "lblNDSoLuong";
+            this.lblNDSoLuong.Size = new System.Drawing.Size(46, 17);
+            this.lblNDSoLuong.TabIndex = 55;
+            this.lblNDSoLuong.Text = "label6";
+            // 
+            // lblNDSLToiThieu
+            // 
+            this.lblNDSLToiThieu.AutoSize = true;
+            this.lblNDSLToiThieu.Location = new System.Drawing.Point(172, 360);
+            this.lblNDSLToiThieu.Name = "lblNDSLToiThieu";
+            this.lblNDSLToiThieu.Size = new System.Drawing.Size(46, 17);
+            this.lblNDSLToiThieu.TabIndex = 56;
+            this.lblNDSLToiThieu.Text = "label7";
+            // 
+            // lblNDGiaBan
+            // 
+            this.lblNDGiaBan.AutoSize = true;
+            this.lblNDGiaBan.Location = new System.Drawing.Point(172, 416);
+            this.lblNDGiaBan.Name = "lblNDGiaBan";
+            this.lblNDGiaBan.Size = new System.Drawing.Size(46, 17);
+            this.lblNDGiaBan.TabIndex = 57;
+            this.lblNDGiaBan.Text = "label8";
+            // 
+            // lblNDGiaNhap
+            // 
+            this.lblNDGiaNhap.AutoSize = true;
+            this.lblNDGiaNhap.Location = new System.Drawing.Point(172, 472);
+            this.lblNDGiaNhap.Name = "lblNDGiaNhap";
+            this.lblNDGiaNhap.Size = new System.Drawing.Size(46, 17);
+            this.lblNDGiaNhap.TabIndex = 58;
+            this.lblNDGiaNhap.Text = "label9";
+            // 
+            // lblNDMoTa
+            // 
+            this.lblNDMoTa.AutoSize = true;
+            this.lblNDMoTa.Location = new System.Drawing.Point(475, 24);
+            this.lblNDMoTa.Name = "lblNDMoTa";
+            this.lblNDMoTa.Size = new System.Drawing.Size(54, 17);
+            this.lblNDMoTa.TabIndex = 59;
+            this.lblNDMoTa.Text = "label11";
+            // 
+            // Form_ChiTietHH
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(976, 552);
+            this.Controls.Add(this.lblNDMoTa);
+            this.Controls.Add(this.lblNDGiaNhap);
+            this.Controls.Add(this.lblNDGiaBan);
+            this.Controls.Add(this.lblNDSLToiThieu);
+            this.Controls.Add(this.lblNDSoLuong);
+            this.Controls.Add(this.lblNDLoaiID);
+            this.Controls.Add(this.lblNDNuocSX);
+            this.Controls.Add(this.lblNDDVT);
+            this.Controls.Add(this.lblNDTenSP);
+            this.Controls.Add(this.lblNDMaSP);
+            this.Controls.Add(this.lblMoTa);
+            this.Controls.Add(this.lblLoaiID);
+            this.Controls.Add(this.lblSLToiThieu);
+            this.Controls.Add(this.lblGiaBan);
+            this.Controls.Add(this.lblDVT);
+            this.Controls.Add(this.lblGiaNhap);
+            this.Controls.Add(this.lblSoLuong);
+            this.Controls.Add(this.lblNuocSX);
+            this.Controls.Add(this.lblTenSP);
+            this.Controls.Add(this.lblMaSP);
+            this.Name = "Form_ChiTietHH";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form_ChiTietHH";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form_ChiTietHH_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLoaiID;
+        private System.Windows.Forms.Label lblSLToiThieu;
+        private System.Windows.Forms.Label lblGiaBan;
+        private System.Windows.Forms.Label lblDVT;
+        private System.Windows.Forms.Label lblGiaNhap;
+        private System.Windows.Forms.Label lblSoLuong;
+        private System.Windows.Forms.Label lblNuocSX;
+        private System.Windows.Forms.Label lblTenSP;
+        private System.Windows.Forms.Label lblMaSP;
+        private System.Windows.Forms.Label lblMoTa;
+        private System.Windows.Forms.Label lblNDMaSP;
+        private System.Windows.Forms.Label lblNDTenSP;
+        private System.Windows.Forms.Label lblNDDVT;
+        private System.Windows.Forms.Label lblNDNuocSX;
+        private System.Windows.Forms.Label lblNDLoaiID;
+        private System.Windows.Forms.Label lblNDSoLuong;
+        private System.Windows.Forms.Label lblNDSLToiThieu;
+        private System.Windows.Forms.Label lblNDGiaBan;
+        private System.Windows.Forms.Label lblNDGiaNhap;
+        private System.Windows.Forms.Label lblNDMoTa;
+    }
+}
---
+++ DO-AN-NHOM-1/App_sale_manager/App_sale_manager/Form_ChiTietHH.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App_sale_manager
+{
+    public partial class Form_ChiTietHH : Form
+    {
+        public Form_ChiTietHH()
+        {
+            InitializeComponent();
+        }
+        public Form_ChiTietHH(string SPID, string TenSP, string SL, string NuocSX, string GiaBan, string GiaNhap, string DVT, string SLTT, string LoaiSP, string MoTa)
+        {
+            InitializeComponent();
+
+            lblNDMaSP.Text = SPID;
+            lblNDTenSP.Text = TenSP;
+            lblNDSoLuong.Text = SL;
+            lblNDNuocSX.Text = NuocSX;
+            lblNDGiaBan.Text = GiaNhap;
+            lblNDGiaNhap.Text = GiaBan;
+            lblNDDVT.Text = DVT;
+            lblNDSLToiThieu.Text = SLTT;
+            lblNDLoaiID.Text = LoaiSP;
+            lblNDMoTa.Text = MoTa;
+
+        }
+
+        private void Form_ChiTietHH_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Form_main_admin frm = new Form_main_admin();
+
+            frm.Show();
+        }
+    }
+}
