534f10d07502b7d73475e00a7beb9ba9be4f8def
Triệu Tuấn Tú <90883273+Vippro67@users.noreply.github.com>
2021-10-30 19:41:04 +0700

    Hàng hoá
    Chức năng thêm xoá sửa xem chi tiết

---
+++ DO-AN-NHOM-1/App_sale_manager/App_sale_manager/Form_login.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace App_sale_manager
+{
+    partial class Form_login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_tiltle_login = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label_usr = new System.Windows.Forms.Label();
+            this.textBox_usr = new System.Windows.Forms.TextBox();
+            this.textBox_passwd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.link_frm_doipasswd = new System.Windows.Forms.LinkLabel();
+            this.btn_dangnhap = new System.Windows.Forms.Button();
+            this.btn_thoat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label_tiltle_login
+            // 
+            this.label_tiltle_login.AutoSize = true;
+            this.label_tiltle_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_tiltle_login.Location = new System.Drawing.Point(178, 109);
+            this.label_tiltle_login.Name = "label_tiltle_login";
+            this.label_tiltle_login.Size = new System.Drawing.Size(211, 25);
+            this.label_tiltle_login.TabIndex = 0;
+            this.label_tiltle_login.Text = "Đăng nhập tài khoản";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(178, 200);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "password";
+            // 
+            // label_usr
+            // 
+            this.label_usr.AutoSize = true;
+            this.label_usr.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_usr.Location = new System.Drawing.Point(180, 163);
+            this.label_usr.Name = "label_usr";
+            this.label_usr.Size = new System.Drawing.Size(37, 18);
+            this.label_usr.TabIndex = 1;
+            this.label_usr.Text = "user";
+            // 
+            // textBox_usr
+            // 
+            this.textBox_usr.Location = new System.Drawing.Point(280, 163);
+            this.textBox_usr.Name = "textBox_usr";
+            this.textBox_usr.Size = new System.Drawing.Size(255, 22);
+            this.textBox_usr.TabIndex = 2;
+            // 
+            // textBox_passwd
+            // 
+            this.textBox_passwd.Location = new System.Drawing.Point(280, 200);
+            this.textBox_passwd.Name = "textBox_passwd";
+            this.textBox_passwd.PasswordChar = '*';
+            this.textBox_passwd.Size = new System.Drawing.Size(255, 22);
+            this.textBox_passwd.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(181, 248);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(273, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "nếu bạn muốn đổi mật khẩu mới hãy nhấn";
+            // 
+            // link_frm_doipasswd
+            // 
+            this.link_frm_doipasswd.AutoSize = true;
+            this.link_frm_doipasswd.Location = new System.Drawing.Point(461, 248);
+            this.link_frm_doipasswd.Name = "link_frm_doipasswd";
+            this.link_frm_doipasswd.Size = new System.Drawing.Size(29, 17);
+            this.link_frm_doipasswd.TabIndex = 4;
+            this.link_frm_doipasswd.TabStop = true;
+            this.link_frm_doipasswd.Text = "link";
+            this.link_frm_doipasswd.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.link_frm_doipasswd_LinkClicked);
+            // 
+            // btn_dangnhap
+            // 
+            this.btn_dangnhap.Location = new System.Drawing.Point(183, 300);
+            this.btn_dangnhap.Name = "btn_dangnhap";
+            this.btn_dangnhap.Size = new System.Drawing.Size(172, 44);
+            this.btn_dangnhap.TabIndex = 5;
+            this.btn_dangnhap.Text = "Đăng nhập";
+            this.btn_dangnhap.UseVisualStyleBackColor = true;
+            this.btn_dangnhap.Click += new System.EventHandler(this.btn_dangnhap_Click);
+            // 
+            // btn_thoat
+            // 
+            this.btn_thoat.Location = new System.Drawing.Point(361, 300);
+            this.btn_thoat.Name = "btn_thoat";
+            this.btn_thoat.Size = new System.Drawing.Size(172, 44);
+            this.btn_thoat.TabIndex = 5;
+            this.btn_thoat.Text = "Thoát";
+            this.btn_thoat.UseVisualStyleBackColor = true;
+            this.btn_thoat.Click += new System.EventHandler(this.btn_thoat_Click);
+            // 
+            // Form_login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 407);
+            this.Controls.Add(this.btn_thoat);
+            this.Controls.Add(this.btn_dangnhap);
+            this.Controls.Add(this.link_frm_doipasswd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_passwd);
+            this.Controls.Add(this.textBox_usr);
+            this.Controls.Add(this.label_usr);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label_tiltle_login);
+            this.Name = "Form_login";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_tiltle_login;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label_usr;
+        private System.Windows.Forms.TextBox textBox_usr;
+        private System.Windows.Forms.TextBox textBox_passwd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel link_frm_doipasswd;
+        private System.Windows.Forms.Button btn_dangnhap;
+        private System.Windows.Forms.Button btn_thoat;
+    }
+}
+
---
+++ DO-AN-NHOM-1/App_sale_manager/App_sale_manager/Form_main_NV.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace App_sale_manager
+{
+    partial class Form_main_NV
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btn_Thoat = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1209, 647);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btn_Thoat);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1201, 618);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 71);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btn_Thoat
+            // 
+            this.btn_Thoat.Location = new System.Drawing.Point(833, 515);
+            this.btn_Thoat.Name = "btn_Thoat";
+            this.btn_Thoat.Size = new System.Drawing.Size(324, 70);
+            this.btn_Thoat.TabIndex = 0;
+            this.btn_Thoat.Text = "Thoat";
+            this.btn_Thoat.UseVisualStyleBackColor = true;
+            this.btn_Thoat.Click += new System.EventHandler(this.btn_Thoat_Click);
+            // 
+            // Form_main_NV
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1213, 653);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form_main_NV";
+            this.Text = "Form_main_NV";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button btn_Thoat;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
